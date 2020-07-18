2e41b022fc82d4757e237f5fe049836f4adf9241
Maxwell Lang <maxwlang@gmail.com>
2020-07-18 03:26:27 -0500

    Reduce config access in bot.js

--- bin/bot.js
+++ bin/bot.js
@@ -3,15 +3,18 @@ const { AkairoClient, } = require('discord-akairo');
 const Sentry = require('@sentry/node');
 
 // Vital local resources
-const config = require('../src/config');
+const {
+    sentry,
+    discord,
+} = require('../src/config');
 const logger = require('../src/modules/winston');
 
 // We've successfully reached our bot core code.
 logger.info('Stylizer starting up!');
 
 // Initialize Sentry error handler if enabled
-if (config.sentry.enabled) {
-    Sentry.init({ dsn: `https://${config.sentry.key}@${config.sentry.ingestNode}.ingest.sentry.io/${config.sentry.project}`, });
+if (sentry.enabled) {
+    Sentry.init({ dsn: `https://${sentry.key}@${sentry.ingestNode}.ingest.sentry.io/${sentry.project}`, });
 } else {
     console.warn('Sentry is not logging errors.');
 }
@@ -27,4 +30,4 @@ class Stylizer extends AkairoClient {
 }
 
 const client = new Stylizer();
-client.login(config.discord.token);
+client.login(discord.token);
