34985770dc6789e1c96663e2a5575cc4edaf98d4
Maxwell Lang <maxwlang@gmail.com>
2020-07-18 14:54:42 -0500

    CStylize template

---
+++ src/commands/cstylize.js
@@ -0,0 +1,28 @@
+/* eslint-disable class-methods-use-this */
+/**
+ * Custom Stylize Command
+ * Stylizes images without pre-rendered models, takes gpu time.
+ */
+const { Command, } = require('discord-akairo');
+
+class CStylize extends Command {
+    constructor() {
+        super('cstylize', {
+            aliases: [
+                'cstylize',
+                'cstyle',
+                'cs',
+            ],
+            cooldown: 60000, // 1 Minute
+            description: 'Stylizes images without a pre-rendered model template, takes GPU time.',
+            // lock: 
+            // locker:
+        });
+    }
+
+    exec(message) {
+        return message.reply('Pong!');
+    }
+}
+
+module.exports = CStylize;
